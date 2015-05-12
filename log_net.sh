00934dbf742634da8862a7158f9185cb2d6c84c6
selby <selby.kendrick@gmail.com>
2015-05-12 18:08:04 -0500

    ze works

--- log_net.sh
+++ log_net.sh
@@ -2,39 +2,30 @@ SYSHOSTNAME=$(hostname)
 SYSUUID=$(/sbin/blkid | grep "$(df -h / | sed -n 2p | cut -d" " -f1):" | grep -o "UUID=\"[^\"]*\" " | sed "s/UUID=\"//;s/\"//")
 CURDATE=$(date +"%s")
 ifstat | awk -v uuid="$SYSUUID" -v date="$CURDATE" -v host="$SYSHOSTNAME" '
-function isnum(x){return(x==x+0)}
 BEGIN {
   print "{";
   print "  \"silent\": true,";
-  print "  \"network\":";
-  print "    [";
+  print "  \"network\": [";
 }
+NR < 4  { next }
 {
-  #PID  PPID PRI  NI STAT USER     TT        SESS %CPU %MEM    SZ   RSS COMMAND         COMMAND
-
-  for( i=1 ; i <=NR ; i++ ) {
-    gsub(/^[ \t]+/,"",$i);
-    gsub(/[ \t]+$/,"",$i);
-  }
-
-  if(!isnum($1)){
-    printf("%s{\"%s\": ", t, $1)
-    for( i=2 ; i <NR ; i++ ) {
+  if(NF == 9){
+    printf("%s    {\"%s\": ", t, $1)
+    for( i=2 ; i <NF ; i++ ) {
       printf(", \"%s\": ", $i)
     }
-    printf(", \"%s\": ", $NR)
+    printf(", \"%s\": ", $NF)
     {t=",\n"}
-  } else {
+  } else if(NF == 8) {
     printf(", \"%s\": ", $1)
-    for( i=2 ; i <NR ; i++ ) {
+    for( i=2 ; i <NF ; i++ ) {
       printf(", \"%s\": ", $i)
     }
-    printf(", \"%s\": }", $NR)
+    printf(", \"%s\": }", $NF)
     {t=",\n"}
   }
 }
-
 END {
-  print "  ]";
+  print "\n  ]";
   print "}";
-}' #| curl --header "Content-Type: application/json" -d @- http://localhost:5005/processes
+}' #| curl --header "Content-Type: application/json" -d @- http://localhost:5005/network
